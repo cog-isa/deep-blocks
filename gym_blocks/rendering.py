3b884549750ee24ec7506c9108840daacdd0429f
eayunts <eayunts@gmail.com>
2017-04-28 13:00:29 +0300

    Added slideshow and image generation

---
+++ gym_blocks/rendering.py
@@ -0,0 +1,26 @@
+# import pygame
+#
+# pygame.init()
+#
+# screen = pygame.display.set_mode((900,900))
+#
+# pygame.display.update()
+# import sys
+#
+# red = (255,0,0)
+# green = (0,255,0)
+# blue = (0,0,255)
+# darkBlue = (0,0,128)
+# white = (255,255,255)
+# black = (0,0,0)
+# pink = (255,200,200)
+#
+# screen.fill(red)
+#
+# pygame.draw.lines(screen, black, False, [(100,100), (150,200), (200,100)], 1)
+#
+# for event in pygame.event.get():
+#     if event.type == pygame.QUIT:
+#         pygame.quit();
+#         sys.exit();
+
